eed4c240fc0bb7c21e30ac1d211d73c32d34bbf5
jondor <jondor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-04 16:30:50 +0000

    checking tag 1.0
    git-svn-id: https://plugins.svn.wordpress.org/addgeorss/tags/1.0@1019609 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ addGEORSS.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package addGEORSS
+ * @version 1.0
+ */
+/*
+Plugin Name: addGEORSS
+Plugin URI: http://www.funsite.eu/addGEORSS
+Description: Adds a GEORSS point to the RSS feed using the GEO information in the featured image
+Author: Gerhard Hoogterp
+Version: 1.0
+Author URI: http://www.funsite.eu/
+*/
+
+if (!function_exists('gps')) {
+	function gps($coordinate, $hemisphere) {
+		for ($i = 0; $i < 3; $i++) {
+		$part = explode('/', $coordinate[$i]);
+		if (count($part) == 1) {
+			$coordinate[$i] = $part[0];
+		} else if (count($part) == 2) {
+			$coordinate[$i] = floatval($part[0])/floatval($part[1]);
+		} else {
+			$coordinate[$i] = 0;
+		}
+		}
+		list($degrees, $minutes, $seconds) = $coordinate;
+		$sign = ($hemisphere == 'W' || $hemisphere == 'S') ? -1 : 1;
+		return $sign * ($degrees + $minutes/60 + $seconds/3600);
+	}
+}
+if (!function_exists('getLocationFromDBorExif')) {
+	function getLocationFromDBorExif($post_thumbnail_id) {
+		// Check if the location is already stored in the database
+		// if not, try to get it from the EXIF information and store it.
+		$location = get_post_meta($post_thumbnail_id,'EXIF_location',true);
+		if (empty($location)) {
+
+			$thumbnail=get_attached_file( $post_thumbnail_id, true );
+			$exif = exif_read_data($thumbnail);
+			if (is_array($exif["GPSLatitude"]) && is_array($exif["GPSLongitude"])) {
+				$location['latitude'] = gps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
+				$location['longitude'] = gps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
+				$location['hasLocation'] = true;
+
+				} else {
+				$location['hasLocation'] = false;
+			}
+
+			add_post_meta($post_thumbnail_id,'EXIF_location',$location) || update_post_meta($post_thumbnail_id,'EXIF_location',$location);
+		}
+	return $location;
+	}
+}
+
+function feed_addNameSpace() {
+	echo 'xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#"'."\n";
+}
+
+function feed_addMeta($for_comments) {
+	$post_thumbnail_id = get_post_thumbnail_id( $GLOBALS['post']->ID );
+	$location = getLocationFromDBorExif($post_thumbnail_id);
+	if (!$for_comments && $location['hasLocation']) {
+		echo "<geo:lat>".$location['latitude']."</geo:lat>\n";
+		echo "<geo:long>".$location['longitude']."</geo:long>\n";
+	}
+}
+
+add_action( "rss2_ns", "feed_addNameSpace" );
+add_action( "rss_item", "feed_addMeta", 5, 1 );
+add_action( "rss2_item", "feed_addMeta", 5, 1 );
+?>
