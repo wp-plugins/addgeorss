00edee6a2a350b5272e2b8b520ecfedc153bf44a
jondor <jondor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-27 21:24:14 +0000

    rewriten to avoid classes with my other gps plugins
    git-svn-id: https://plugins.svn.wordpress.org/addgeorss/tags/1.1@1055219 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ addGEORSS.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @package addGEORSS
+ * @version 1.1
+ */
+/*
+Plugin Name: addGEORSS
+Plugin URI: http://www.funsite.eu/addGEORSS
+Description: Adds a GEORSS point to the RSS feed using the GEO information in the featured image
+Author: Gerhard Hoogterp
+Version: 1.1
+Author URI: http://www.funsite.eu/
+*/
+
+class addgeorss {
+
+	const FS_TEXTDOMAIN = 'addgeorss';
+	
+    public function __construct() {
+		add_action('init', array($this,'myTextDomain'));
+		add_filter('plugin_row_meta', array($this,'addgeorss_PluginLinks'),10,2);
+		add_action( "rss2_ns", array($this,"feed_addNameSpace") );
+		add_action( "rss_item", array($this,"feed_addMeta"), 5, 1 );
+		add_action( "rss2_item", array($this,"feed_addMeta"), 5, 1 );
+    }
+
+	function gps($coordinate, $hemisphere) {
+		for ($i = 0; $i < 3; $i++) {
+		$part = explode('/', $coordinate[$i]);
+		if (count($part) == 1) {
+			$coordinate[$i] = $part[0];
+		} else if (count($part) == 2) {
+			$coordinate[$i] = floatval($part[0])/floatval($part[1]);
+		} else {
+			$coordinate[$i] = 0;
+		}
+		}
+		list($degrees, $minutes, $seconds) = $coordinate;
+		$sign = ($hemisphere == 'W' || $hemisphere == 'S') ? -1 : 1;
+		return $sign * ($degrees + $minutes/60 + $seconds/3600);
+	}
+
+	function getLocationFromDBorExif($post_thumbnail_id) {
+		// Check if the location is already stored in the database
+		// if not, try to get it from the EXIF information and store it.
+		$location = get_post_meta($post_thumbnail_id,'EXIF_location',true);
+		if (empty($location)) {
+
+			$thumbnail=get_attached_file( $post_thumbnail_id, true );
+			$exif = exif_read_data($thumbnail);
+			if (is_array($exif["GPSLatitude"]) && is_array($exif["GPSLongitude"])) {
+				$location['latitude'] = $this->gps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
+				$location['longitude'] = $this->gps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
+				$location['hasLocation'] = true;
+
+				} else {
+				$location['hasLocation'] = false;
+			}
+
+			add_post_meta($post_thumbnail_id,'EXIF_location',$location) || update_post_meta($post_thumbnail_id,'EXIF_location',$location);
+		}
+	return $location;
+	}
+
+	function feed_addNameSpace() {
+		echo 'xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#"'."\n";
+	}
+
+	function feed_addMeta($for_comments) {
+		$post_thumbnail_id = get_post_thumbnail_id( $GLOBALS['post']->ID );
+		$location = $this->getLocationFromDBorExif($post_thumbnail_id);
+		if (!$for_comments && $location['hasLocation']) {
+			echo "<geo:lat>".$location['latitude']."</geo:lat>\n";
+			echo "<geo:long>".$location['longitude']."</geo:long>\n";
+		}
+	}
+
+	function addgeorss_PluginLinks($links, $file) {
+		$base = plugin_basename(__FILE__);
+		if ($file == $base) {
+			$links[] = '<a href="https://wordpress.org/support/view/plugin-reviews/addgeorss">' . __('A review would be appriciated.',self::FS_TEXTDOMAIN) . '</a>';
+		}
+		return $links;
+	}
+
+	function myTextDomain() {
+		load_plugin_textdomain(
+			self::FS_TEXTDOMAIN,
+			false,
+			dirname(plugin_basename(__FILE__)).'/languages/'
+		);
+	}
+	
+}
+
+$addgeorss = new addgeorss();
+?>
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Plugin Name ===
+Contributors: jondor
+Donate link: http://www.funsite.eu/downloadable-wallpapers/
+Tags: featured image,gps,rss,georss
+Requires at least: 3.0.1
+Tested up to: 4.1
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin adds an GEORSS latitude/longitude based on the GPS info in the featured image when available.
+
+== Description ==
+
+This plugin adds an GEORSS latitude/longitude based on the GPS info in the featured image when available.
+The GEO info is also stored in the database. This works together with my other GPS_MAP_widget plugin in that it uses the same metakey.
+The image file is only read once.
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Frequently Asked Questions ==
+
+= Why?? =
+Why not? Lots of systems can read GEORSS and add it to a map. 
+
+== Screenshots ==
+
+1. Example of georss tags
+
+== Changelog ==
+
+= 1.1 =
+Rewriten to classbased plugin. Fixes some clashes with my other plugins.
+The plugin is now translatable, but since there's only 1 single line begging for a review, I didn't really bother to provide the dutch translation.
+
+= 1.0 =
+* First release
+
+== Upgrade Notice ==
+
+Nothing  yet. 
+
